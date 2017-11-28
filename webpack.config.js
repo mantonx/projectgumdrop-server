051efb3891b855a91324236237e787dd77a1146a
Matt Panton <matthew.panton@gmail.com>
2017-11-28 11:15:33 -0500

    Updated webpack to include plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+const webpack = require('webpack');
 const path = require('path');
+const yargs = require('yargs');
 const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const argv = yargs
+.boolean('webpackstats')
+.argv
+
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -10,6 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: [
           'imports-loader?graphql',
           {
@@ -27,4 +39,41 @@ module.exports = {
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HardSourceWebpackPlugin(),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        mangle: true,
+        ie8: false,
+        ecma: 8,
+        compress: {
+          warnings: false,
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+        },
+        output: {
+          comments: false,
+        },
+        exclude: [/\.min\.js$/gi],
+      },
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0
+    }),
+  ],
 };
+
+if (argv.webpackstats) {
+  module.exports.plugins.push(
+    new BundleAnalyzerPlugin()
+  );
+}
